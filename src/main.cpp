9a1897d5b3a2f1636aefd20eaa2686454c27c732
Alex Li <islicy@outlook.com>
2023-04-25 19:26:14 -0400

    before final adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -13,19 +13,55 @@ Timer timer;
 void tft_init()
 {
   TFT.claim(stdout); // send stdout to the TFT display
-  TFT.set_orientation(1);
+
+  TFT.background(Black); // set background to black
+  TFT.foreground(White); // set chars to white
+  TFT.cls();             // clear the screen
+
   TFT.background(Black);
-  TFT.foreground(White);
   TFT.cls();
+
+  TFT.cls();
+  TFT.rect(0, 0, 400, 400, LightGrey);
+
   TFT.set_font((unsigned char *)Arial28x28);
   TFT.locate(15, 10);
-  TFT.printf("Press enter to start.");
+  TFT.printf("ECE-GY 6483");
+
+  TFT.foreground(Red);
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 50);
+  TFT.printf("Guesture");
+
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 90);
+  TFT.printf("Unlock");
+
+  TFT.foreground(Blue);
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 130);
+  TFT.printf("Alex");
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 180);
+  TFT.printf("Alhad");
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 220);
+  TFT.printf("Aman, S");
+  TFT.set_font((unsigned char *)Arial28x28);
+  TFT.locate(15, 260);
+  TFT.printf("Aman, V");
+
+  wait_us(2000000);
+
+  TFT.cls();
+  TFT.rect(0, 0, 400, 400, LightGrey);
+  TFT.foreground(White); // set chars to white
 }
 
 void tft_disp(const char *mode)
 {
   TFT.claim(stdout); // send stdout to the TFT display
-  TFT.set_orientation(1);
+  TFT.set_orientation(0);
   TFT.background(Black);
   TFT.foreground(White);
   TFT.cls(); // 清除屏幕上的文本
@@ -45,8 +81,7 @@ DigitalOut led_indicator(LED1);
 InterruptIn enter_key(USER_BUTTON);
 volatile bool record_mode = false;
 volatile bool unlock_mode = false;
-volatile bool init_mode = true;
-
+volatile bool button_pressed = false;
 // x,y,z rotation values from gyro
 int16_t final_x, final_y, final_z = 0;
 
@@ -61,8 +96,8 @@ float recorded_sequence[max_sequence_length][3]; // Store x, y, and z values
 uint32_t recorded_timestamps[max_sequence_length];
 int sequence_length = 0;
 
-// Tolerance for gesture comparison
-const float tolerance = 0.05;
+// Tolerance for gesture comparison (calculating the difference between two angles)
+const float tolerance = 20.0;
 
 // Function prototypes
 void gyro_init(); // Function to initialize L3GD20 gyro
@@ -78,33 +113,36 @@ int main()
   tft_init();
   gyro_init();
   timer.start();
+
+  auto last_button_press_time = std::chrono::steady_clock::now();
+
   while (1)
   {
-    ThisThread::sleep_for(chrono::milliseconds(100));
-    printf("init_mode: %d\n", init_mode);
-    printf("record_mode: %d\n", record_mode);
-    printf("unlock_mode: %d\n", unlock_mode);
-    ; // Add a wait to control the loop rate
+    auto now = std::chrono::steady_clock::now();
+    auto time_since_last_press = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_button_press_time).count();
+
+    if (button_pressed && time_since_last_press > 200)
+    {
+      enter_key_handler();
+      last_button_press_time = now;
+    }
+
     gyro_read();
 
-    if (!init_mode)
+    if (record_mode)
     {
-      if (record_mode)
+      record_sequence();
+    }
+    else if (unlock_mode)
+    {
+      bool match = compare_sequence();
+      if (match)
       {
-        tft_disp("Recording Mode");
-        record_sequence();
+        led_indicator = 1;
       }
-      else if (unlock_mode)
+      else
       {
-        bool match = compare_sequence();
-        if (match)
-        {
-          led_indicator = 1;
-        }
-        else
-        {
-          led_indicator = 0;
-        }
+        led_indicator = 0;
       }
     }
   }
@@ -112,12 +150,12 @@ int main()
 
 void enter_key_handler()
 {
-  if (init_mode)
+  if (!button_pressed)
   {
-    init_mode = false;
-    tft_disp("Press button to start");
+    button_pressed = true;
+    return;
   }
-  else if (!record_mode && !unlock_mode)
+  if (!record_mode && !unlock_mode)
   {
     record_mode = true;
     sequence_length = 0;
@@ -138,11 +176,8 @@ void enter_key_handler()
     led_indicator = 0;
     tft_disp("Unlock Mode Ended");
   }
-  // Add a small delay to debounce the button press
-  ThisThread::sleep_for(chrono::milliseconds(200));
+  button_pressed = false;
 }
-
-
 
 void record_sequence()
 {
@@ -152,6 +187,7 @@ void record_sequence()
     recorded_sequence[sequence_length][1] = datay;
     recorded_sequence[sequence_length][2] = dataz;
     sequence_length++;
+    wait_us(200000); // Add a 200 ms delay between each recorded data point
   }
   else
   {
@@ -159,31 +195,56 @@ void record_sequence()
     led_indicator = 0;
     tft_disp("Record Mode Ended (Max Length)");
   }
+  if (sequence_length > 0)
+  {
+    recorded_timestamps[sequence_length - 1] = timer.read_ms() - recorded_timestamps[sequence_length - 1];
+  }
   recorded_timestamps[sequence_length] = timer.read_ms();
 }
 
+// 修改compare_sequence()函数
 bool compare_sequence()
 {
-  bool match = true;
+  int matched_points = 0;
+  float match_percentage = 0.0;
+  const float match_threshold = 0.8; // 80% match required to unlock
+
   timer.reset();
+  uint32_t previous_time = 0;
   for (int i = 0; i < sequence_length; i++)
   {
-    uint32_t time_to_wait = recorded_timestamps[i] - std::chrono::duration_cast<std::chrono::milliseconds>(timer.elapsed_time()).count();
+    uint32_t current_time = timer.read_ms();
+    uint32_t time_interval = current_time - previous_time;
+    previous_time = current_time;
+
+    uint32_t time_to_wait = recorded_timestamps[i] - time_interval;
     if (time_to_wait > 0)
     {
       ThisThread::sleep_for(chrono::milliseconds(time_to_wait));
     }
     gyro_read(); // Read the gyro data
-    if (!(fabs(datax - recorded_sequence[i][0]) < tolerance &&
-          fabs(datay - recorded_sequence[i][1]) < tolerance &&
-          fabs(dataz - recorded_sequence[i][2]) < tolerance))
+    if (!(fabs(datax - recorded_sequence[i][0]) > tolerance ||
+          fabs(datay - recorded_sequence[i][1]) > tolerance ||
+          fabs(dataz - recorded_sequence[i][2]) > tolerance))
     {
-      match = false;
-      break;
+      matched_points++;
     }
   }
-  return match;
+
+  match_percentage = (float)matched_points / sequence_length;
+
+  if (match_percentage >= match_threshold)
+  {
+    led_indicator = 1;
+    return true;
+  }
+  else
+  {
+    led_indicator = 0;
+    return false;
+  }
 }
+
 
 void gyro_init()
 {
